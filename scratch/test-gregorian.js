039e598def9f2232d517182d1a3494697f26d635
md-ali-0 <mohammad.98482@gmail.com>
2026-04-27 11:49:36 +0600

    chore: remove scratch test file

--- scratch/test-gregorian.js
+++
@@ -1,6 +0,0 @@
-import { convertToEnglishDate } from "../dist/calendars/english/converter.js";
-
-const date = new Date(2026, 3, 6, 17, 59); // April 6, 2026, 5:59 PM
-const result = convertToEnglishDate(date, "bn");
-
-console.log("English Date (BN):", `${result.day} ${result.month} ${result.year} | ${result.period} ${result.hour}:${result.minute}`);
